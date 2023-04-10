a80832c1482a3d43e92564dcda84969158f3c48f
Robert Safin <robertsafin98@gmail.com>
2023-04-10 22:59:53 +0800

    render api data

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -2,15 +2,15 @@ import { FC } from "react";
 
 interface Props {
   title: string;
-  content: string;
+  description: string;
 }
 
-const BlogCard: FC<Props> = ({ title, content }) => {
+const BlogCard: FC<Props> = ({ title, description }) => {
   return (
     <div className="max-w-3xl mx-auto p-5">
       <div className="bg-red-300  p-2 rounded">
         <h1 className="text-gray-900 text-3xl font-mono">{title}</h1>
-        <p className="text-gray-600">{content}</p>
+        <p className="text-gray-600">{description}</p>
       </div>
     </div>
   );
--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -1,31 +1,29 @@
 import { NextPage } from "next";
 import BlogCard from "@/components/BlogCard";
+import { useEffect, useState } from "react";
 
-interface Props {
-
-}
+interface Props {}
 
 const Blogs: NextPage<Props> = () => {
+  const [posts, setPosts] = useState<
+    { title: string; slug: string; meta: string }[]
+  >([]);
+
+  const fetchPosts = async () => {
+    const data = await fetch("/api/posts");
+    const res = await data.json();
+    setPosts(res.postInfo);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <div className="max-w-3xl mx-auto p-5 space-y-5">
-      <BlogCard
-        title={"this is blog"}
-        content={
-          "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora cumque minus neque ratione. Iusto ipsum tenetur, saepe velit accusamus voluptatibus."
-        }
-      />
-      <BlogCard
-        title={"this is blog"}
-        content={
-          "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora cumque minus neque ratione. Iusto ipsum tenetur, saepe velit accusamus voluptatibus."
-        }
-      />
-      <BlogCard
-        title={"this is blog"}
-        content={
-          "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora cumque minus neque ratione. Iusto ipsum tenetur, saepe velit accusamus voluptatibus."
-        }
-      />
+      {posts.map((post) => (
+        <BlogCard title={post.title} description={post.meta} key={post.meta}/>
+      ))}
     </div>
   );
 };
