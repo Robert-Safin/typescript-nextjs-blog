6bf3251a405ed01d81d18271dcc8cdc9bf355a10
Robert Safin <robertsafin98@gmail.com>
2023-04-11 13:16:47 +0800

    fix types

--- src/pages/blogs/[postSlug].tsx
+++ src/pages/blogs/[postSlug].tsx
@@ -1,77 +1,74 @@
 import { GetStaticPaths, InferGetStaticPropsType, NextPage } from "next";
-import { GetStaticProps } from "next";
-import matter from 'gray-matter'
-import fs from 'fs'
-import path from 'path'
-import { ParsedUrlQuery } from 'querystring'
+import { GetStaticProps, GetStaticPathsContext } from "next";
+import matter from 'gray-matter';
+import fs from 'fs';
+import path from 'path';
+import { ParsedUrlQuery } from 'querystring';
 
+interface Post {
+  title: string;
+  content: string;
+}
 
-type Post = InferGetStaticPropsType<typeof getStaticPaths>
+interface SinglePageProps {
+  post: Post;
+}
 
-const SinglePage: NextPage<Post> = (props) => {
+interface Params extends ParsedUrlQuery {
+  postSlug: string;
+}
 
-
-  return(
+const SinglePage: NextPage<SinglePageProps> = ({ post }) => {
+  return (
     <>
-    <h1>{props.post.title}</h1>
-    <p>{props.post.content}</p>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
     </>
-  )
-}
-
-export const getStaticPaths: GetStaticPaths = () => {
-
-  const dirPathToRead = path.join(process.cwd(), '/src/posts')
-
-  const dirs = fs.readdirSync(dirPathToRead)
-  dirs.shift()
-
-
-  const paths = dirs.map((filename) => {
-    const filePathToRead = path.join(process.cwd(), '/src/posts/' + filename)
-    const fileContent = fs.readFileSync(filePathToRead, {encoding: 'utf-8'})
-    return {params: {postSlug: matter(fileContent).data.slug}}
-  })
-
-  return {
-    paths: paths,
-    fallback: false
-  }
-
-}
-
-
-interface IStaticProps extends ParsedUrlQuery {
-  postSlug: string
-}
-
-type PostType = {
-  post: {
-    title: string
-    content: string
-  }
-}
-
-
-export const getStaticProps: GetStaticProps<PostType> = async (context) => {
-  const {params} = context
-  const {postSlug} = params as IStaticProps
-
-
-
-
-  const filePathToRead = path.join(process.cwd(), '/src/posts/' + postSlug + '.md')
-  const fileContent = fs.readFileSync(filePathToRead, {encoding: 'utf-8'})
-  const {content, data} = matter(fileContent)
-
-
-  return {
-    props: {
-      post: {content: content, title: data.title}
-    },
-  };
+  );
 };
 
 
 
-export default SinglePage
+
+
+
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+  const dirPathToRead = path.join(process.cwd(), '/src/posts');
+
+  const dirs = fs.readdirSync(dirPathToRead);
+  dirs.shift();
+
+  const paths = dirs.map((filename) => {
+    const filePathToRead = path.join(process.cwd(), '/src/posts/' + filename);
+    const fileContent = fs.readFileSync(filePathToRead, { encoding: 'utf-8' });
+    return { params: { postSlug: matter(fileContent).data.slug } };
+  });
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<SinglePageProps, Params> = async (context) => {
+  const { params } = context;
+
+  if (!params) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const { postSlug } = params;
+  const filePathToRead = path.join(process.cwd(), '/src/posts/' + postSlug + '.md');
+  const fileContent = fs.readFileSync(filePathToRead, { encoding: 'utf-8' });
+  const { content, data } = matter(fileContent);
+
+  return {
+    props: {
+      post: { content: content, title: data.title },
+    },
+  };
+};
+
+export default SinglePage;
